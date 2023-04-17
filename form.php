9afcf13fa28c0b08220f0afc131dcf9c4eecb061
ET3317 <121935854+ET3317@users.noreply.github.com>
2023-04-17 20:12:23 +0200

    delete few comments

--- form.php
+++ form.php
@@ -1,28 +1,25 @@
 <?php
-// Je vérifie que le formulaire est soumis
-
-
-// Je vérifie si le formulaire est soumis comme d'habitude
+// Je vérifie si le formulaire est soumis 
 if($_SERVER['REQUEST_METHOD'] === "POST"){
-    // Securité en php
-    // chemin vers un dossier sur le serveur qui va recevoir les fichiers uploadés
+   
+    // chemin vers un dossier sur serveur pour fichiers uploadés
     $uploadDir = 'public/upload/';
-    // le nom de fichier sur le serveur est ici généré à partir du nom de fichier sur le poste du client
+    //  nom fichier généré à partir du poste user
     $uploadFile = $uploadDir . basename($_FILES['avatar']['name']);
-    // Je récupère l'extension du fichier
+    // récupération extension du fichier (path + name)
     $extension = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
-    // Les extensions autorisées
+    // extensions autorisées
     $authorizedExtensions = ['jpg','jpeg','png', 'gif', 'webp'];
-    // Le poids max géré par PHP par défaut est de 2M
+    // Le poids max (défaut 2mo)
     $maxFileSize = 1000000;
     $errors=[];
-    // Je sécurise et effectue mes tests
-    /****** Si l'extension est autorisée *************/
+ 
+    /****** if extension est ok *************/
     if( (!in_array($extension, $authorizedExtensions))){
-        $errors[] = 'Veuillez sélectionner une image de type Jpg ou Jpeg ou Png ou Gif ou Webp !';
+        $errors[] = 'Veuillez sélectionner une image de Jpeg, Png, Gif ou Webp !';
     }
 
-    /****** On vérifie si l'image existe et si le poids est autorisé en octets *************/
+    /****** if image existe && si poids max ok *************/
     if( file_exists($_FILES['avatar']['tmp_name']) && filesize($_FILES['avatar']['tmp_name']) > $maxFileSize)
     {
         $errors[] = "Votre fichier doit faire moins de 1M !";
